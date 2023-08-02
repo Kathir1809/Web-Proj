340f514dc9fa775f40839c3fccf909dd8901213a
Kathir1809 <ceokathir@gmail.com>
2023-08-02 23:02:50 +0530

    main

--- jw/settings.py
+++ jw/settings.py
@@ -232,11 +232,11 @@ AWS_S3_REGION_NAME = "us-east-005"
 
 AWS_S3_ENDPOINT_URL = "https://s3.us-east-005.backblazeb2.com"
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID') 
 
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') 
 
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME') 
 
 
 
